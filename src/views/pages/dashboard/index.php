a9098aaf7552cbe86d06f9785f2956f2ee4df3e8
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-25 17:42:48 +0700

    init: pages

---
+++ src/views/pages/dashboard/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/navbar.css">
+</head>
+<body>
+    <h2>Dashboard</h2>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsa nulla minima commodi iusto accusantium vel maxime debitis ab quod autem voluptatum doloremque corrupti, ullam, odio eius? Accusantium, inventore unde.
+
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat at consequuntur cum corrupti iste dicta et perspiciatis veniam voluptatum explicabo quis, aperiam maiores praesentium error. Pariatur aut porro nemo molestias!
+
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio corrupti tempora magni fugiat, facere architecto laboriosam nesciunt quis dicta, consequatur veniam perferendis, itaque debitis sequi eligendi deserunt voluptas possimus eveniet.
+    </p>
+    
+</body>
+</html>
---
+++ src/views/pages/errors/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error Page</title>
+</head>
+<body>
+    <div class="error-container">
+        <h1>Error: <?php echo $data[0]; ?></h1>
+        <p>Sorry, something went wrong.</p>
+    </div>
+</body>
+</html>
