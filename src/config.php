614d66f9a0e2cc1f69e44766bed053e0714d69e4
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-25 16:43:20 +0700

    feat: app configurations

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('SRC_DIR', __DIR__ . '/');
+
+define('CONTROLLER_DIR', SRC_DIR . 'controllers/');
+define('SERVICES_DIR', SRC_DIR . 'services/');
+define('MODELS_DIR', SRC_DIR . 'models/');
+define('VIEWS_DIR', SRC_DIR . 'views/');
+define('PAGES_DIR', SRC_DIR . 'views/pages/');
+
+define("SUCCESS", "SUCCESS");
+
+define("EMAIL_ALREADY_EXISTED", "EMAIL_ALREADY_EXISTED");
+define("USER_NOT_FOUND", "USER_NOT_FOUND");
+define("PASSWORD_INCORRECT", "PASSWORD_INCORRECT");
+define("USER_ALREADY_EXISTS", "USER_ALREADY_EXISTS");
