449b93ee94c8924622d6a21a6deeb3e2d5f6fda0
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:19:05 +0700

    chore: remove notification stuffs

--- public/index.php
+++ public/index.php
@@ -1,20 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../src/init.php';
-require_once __DIR__ . '/../src/views/components/notification.php';
 
-// Start the session if it's not already started
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
-}
-
-// Check if the notification message is set
-if (isset($_SESSION['notification'])) {
-    // Display the notification
-    echo Notification::show($_SESSION['notification']);
-
-    // Unset the notification message so it doesn't keep showing up on refresh
-    unset($_SESSION['notification']);
 }
 
 $app = new App();
