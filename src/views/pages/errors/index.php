9a248eb932dce6ad6de3251656e17b0f818820b3
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:21:53 +0700

    refactor: change var from data to code chore: add back to home button

--- src/views/pages/errors/index.php
+++ src/views/pages/errors/index.php
@@ -5,8 +5,12 @@
 </head>
 <body>
     <div class="error-container">
-        <h1>Error: <?php echo $data[0]; ?></h1>
+        <h1>Error: <?php echo $code; ?></h1>
         <p>Sorry, something went wrong.</p>
+
+        <a href="/">
+            <button>Back to Home</button>
+        </a>
     </div>
 </body>
 </html>
