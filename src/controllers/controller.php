06573bd3eb531bc8683fa54cc4d1091d08b3e173
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:20:33 +0700

    refactor: loadPageNotFound to methodNotAllowed chore: remove showNotification

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once VIEWS_DIR . 'components/notification.php';
-
 class Controller {
     public function view($view, $data = []) {
         // Extract the data array to variables for use in the view
@@ -11,21 +9,7 @@ class Controller {
         require_once PAGES_DIR . $view . '.php';
     }
 
-    public function loadPageNotFound() {
-        $this->view('errors/index');
+    public function methodNotAllowed() {
+        $this->view('errors/index', ['code' => 405]);
     }
-
-    public function showNotification($message) {
-        // Start the session if it's not already started
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-    
-        // Store the notification message in a session variable
-        $_SESSION['notification'] = $message;
-    
-        // Redirect to the dashboard
-        header("Location: /dashboard");
-    }
-    
 }
