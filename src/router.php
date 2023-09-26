fc2aa282f05820f968753de55a318cf366174f06
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:17:30 +0700

    chore: implement role checking before redirecting

--- src/router.php
+++ src/router.php
@@ -4,14 +4,30 @@ class Router {
     private $routes = [];
 
     public function define($routes) {
-        $this->routes = $routes;
+        foreach ($routes as $uri => $data) {
+            $this->routes[$uri] = [
+                'controller' => $data[0],
+                'roles' => isset($data[1]) ? $data[1] : []
+            ];
+        }
     }
 
-    public function direct($uri) {
+    public function direct($uri, $role) {
+        // If the user is logged in and tries to access the home, user, or register routes, redirect them to the dashboard
+        if ($role !== null && in_array($uri, ['', 'login', 'register'])) {
+            header('Location: /dashboard');
+            exit();
+        }
+
         if (array_key_exists($uri, $this->routes)) {
-            return $this->routes[$uri];
+            if (empty($this->routes[$uri]['roles']) || in_array($role, $this->routes[$uri]['roles'])) {
+                return $this->routes[$uri]['controller'];
+            } else {
+                return 'errors';
+            }
         }
 
         return 'errors';
     }
+
 }
