a5f2584f5072c8be6149e1fe936aecaf87649abc
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:20:52 +0700

    refactor: use best practice to pass var

--- src/controllers/errors_controller.php
+++ src/controllers/errors_controller.php
@@ -4,6 +4,6 @@ require_once 'controller.php';
 
 class Errors extends Controller {
     public function index($code = '404') {
-        $this->view('errors/index', [$code]);
+        $this->view('errors/index', ['code' => $code]);
     }
 }
