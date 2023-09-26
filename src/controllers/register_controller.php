67876983cde2fc3a42b05ec1c00e0e8fc1d88c54
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:24:39 +0700

    chore: echo json_encode to connect with xhr refactor: RegisterService to UserService

--- src/controllers/register_controller.php
+++ src/controllers/register_controller.php
@@ -1,8 +1,7 @@
 <?php
 
 require_once 'controller.php';
-require_once SERVICES_DIR . 'register/index.php';
-require_once SERVICES_DIR . 'login/index.php';
+require_once SERVICES_DIR . 'user/index.php';
 
 class Register extends Controller {
     public function index() {
@@ -14,7 +13,7 @@ class Register extends Controller {
                 $this->register();
                 break;
             default:
-                $this->loadPageNotFound();
+                $this->methodNotAllowed();
                 break;
         }
     }
@@ -24,33 +23,50 @@ class Register extends Controller {
     }
 
     private function register() {
-        if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm_password"])) {
-            $username = trim($_POST["username"]);
-            $email = trim($_POST["email"]);
-            $password = trim($_POST["password"]);
-            $confirm_password = trim($_POST["confirm_password"]);
-    
-            if ($password !== $confirm_password) {
-                $this->showNotification("PASSWORD NOT MATCHED");
-            }
+        // Set the content type to application/json
+        header('Content-Type: application/json');
 
-            $registerService = new RegisterService();
-            $registerResult = $registerService->register($username, $email, $password);
-
-            $this->showNotification($registerResult);   
-
-            if ($registerResult == 'SUCCESS') {
-                // Log this user in
-                $loginService = new LoginService();
-                $loginResult = $loginService->login($email,$password);
-
-                header('Location: /dashboard');
-                $this->showNotification($loginResult);
-            } else {
-                // Redirect to Register
-            }
-        } else {
-            $this->showNotification("Please fill in all the required fields!");
+        // Check if all required fields are set
+        if (!isset($_POST["username"]) || !isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["confirm_password"])) {
+            echo json_encode(['status' => 'ERROR', 'message' => 'Please fill in all the required fields!']);
+            exit();
         }
+
+        // Sanitize user input
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        $confirm_password = filter_input(INPUT_POST, 'confirm_password', FILTER_SANITIZE_STRING);
+
+        // Check if password and confirmation password match
+        if ($password !== $confirm_password) {
+            echo json_encode(['status' => 'ERROR', 'message' => 'Password and confirmation password not matched!']);
+            exit();
+        }
+
+        // Register the user
+        $userService = new UserService();
+        $registerResult = $userService->register($username, $email, $password);
+
+        switch ($registerResult) {
+            case 'SUCCESS':
+                // Log this user in
+                $loginResult = $userService->login($email,$password);
+
+                if ($loginResult == 'SUCCESS') {
+                    echo json_encode(['status' => 'SUCCESS', 'message' => 'Successfully registered user! Logged in!']);
+                } else {
+                    echo json_encode(['status' => 'ERROR', 'message' => 'Successfully registered user! Failed to log in!']);
+                }
+                break;
+            case 'EMAIL_ALREADY_EXISTED':
+                echo json_encode(['status' => 'ERROR', 'message' => 'Email already existed.']);
+                break;
+            default:
+                echo json_encode(['status' => 'ERROR', 'message' => 'An error occurred during registration.']);
+                break;
+        }
+
+        exit();
     }
 }
