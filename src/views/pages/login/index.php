a5ea0f2830fc7a9ac9c6dbf6b0b463f5ee205ee5
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:22:31 +0700

    chore: add error message div, connect to css and js

--- src/views/pages/login/index.php
+++ src/views/pages/login/index.php
@@ -4,17 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
     <h1>Login</h1>
 
-    <form method="POST" action="/login">
+    <form id="login-form" method="POST" action="/login">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br><br>
 
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br><br>
+
+        <div id="login-error-message" class="error-message"></div>
 
         <input type="submit" value="Submit">
     </form>
@@ -28,5 +30,7 @@
             Back to Home
         </button>   
     </a>
+
+    <script src="/js/login.js"></script>
 </body>
 </html>
