0c23e0348b58cfee7c33c42e1a72b18abd4122e9
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-27 03:13:23 +0700

    delete: notification

--- public/js/notification.js
+++
@@ -1,9 +0,0 @@
-function showNotification() {
-  var notification = document.getElementById("notification");
-  notification.style.display = "block";
-  setTimeout(function () {
-    notification.style.display = "none";
-  }, 5000);
-}
-
-showNotification();
