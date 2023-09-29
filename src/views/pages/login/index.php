d7ed470071fa479e44d39698a9b6b34de81d6139
auliamey <auliaannandya@gmail.com>
2023-09-30 00:19:14 +0700

    fix login page

--- src/views/pages/login/index.php
+++ src/views/pages/login/index.php
@@ -5,31 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/login.css">
 </head>
-<body>
-    <h1>Login</h1>
+<body id="grad">
+    <div class="center">
+        <div class="container">
+            <h1 class="title">Login</h1>
 
-    <form id="login-form" method="POST" action="/login">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+            <form id="login-form" method="POST" action="/login">
+                <label class="label-text" for="email">Email:</label> <br>
+                <input class="style-input" type="email" id="email" name="email" placeholder="john@example.com" required><br><br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+                <label class="label-text" for="password">Password:</label> <br>
+                <input class="style-input" type="password" id="password" name="password" placeholder="********"required><br><br>
 
-        <div id="login-error-message" class="error-message"></div>
+                <div id="login-error-message" class="error-message"></div>
 
-        <input type="submit" value="Submit">
-    </form>
+                <div class="center-submit">
+                    <input class="sbmt-style" type="submit" value="Submit">
+                </div>
+                
 
-    Haven't have any account? Sign up <a href="/register">here</a>!
-     
-    <br><br>
+            </form>
 
-    <a href="/">
-        <button>
-            Back to Home
-        </button>   
-    </a>
+            <div class="plain-text">
+                Don't have an account? <a class="link" href="/register">Sign up here!</a>
+            </div>
+
+            <a class="center-back" href="/">
+                Back to Home
+            </a>
+        </div>
+    </div>
 
     <script src="/js/login.js"></script>
 </body>
