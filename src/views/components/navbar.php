913780c03032f9bbd15c754c4804014a5dbb4c79
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-29 17:02:09 +0700

    feat: navbar component

--- src/views/components/navbar.php
+++ src/views/components/navbar.php
@@ -1,13 +1,22 @@
 <?php
+function navbar($display_search, $profile_url) {
+    $search_bar = '';
+    if ($display_search) {
+        $search_bar = <<<EOT
+        <input type="text" placeholder="Search..." class="search-bar">
+EOT;
+    }
 
-echo '
+    $navbar = <<<EOT
     <div class="navbar">
-        <h1>Puisiku</h1>
-        <a href="/login">
-            <button>Login</button>
-        </a>
+        <div class="profile-button">
+            <a href="/profile">
+                <img src="$profile_url" alt="Profile" class="profile-image">
+            </a>
+        </div>
+        $search_bar
     </div>
-'
-?>
+EOT;
 
-
+    echo $navbar;
+}
