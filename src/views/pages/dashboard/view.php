c9884083d8e3ec7f982f7d045e1df7a6e0e9cec1
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-09-29 17:07:21 +0700

    chore: dummy view for dashboard

---
+++ src/views/pages/dashboard/view.php
@@ -0,0 +1,4 @@
+<div class="dashboard">
+    <!-- Only for testing, should not get username here, do it in controller -->
+    <h1>Hello <?php echo $_SESSION['username']?>! Welcome to dashboard!</h1>
+</div>
