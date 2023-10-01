59702fe8a8f9abd60699b509d6cf91887ebbed9f
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-01 20:31:12 +0700

    chore: handle routing with method e.g. poems/search

--- src/router.php
+++ src/router.php
@@ -16,7 +16,6 @@ class Router {
         // Parse the URI and separate the path and query string
         $parsedUrl = parse_url($uri);
         $path = trim($parsedUrl['path'], '/');
-        $queryString = isset($parsedUrl['query']) ? $parsedUrl['query'] : '';
 
         // If the user is logged in and tries to access the home, user, or register routes, redirect them to the dashboard
         if ($role !== null && in_array($path, ['', 'login', 'register'])) {
@@ -31,7 +30,7 @@ class Router {
                 $pattern = preg_replace('#:([\w]+)#', '(?P<$1>[\w-]+)', $routePath);
                 $pattern = '#^' . $pattern . '$#';
 
-                // If the pattern matches, return the controller
+                // If the pattern matches, return the controller and method
                 if (preg_match($pattern, $path, $matches)) {
                     // Store the dynamic parameters in $_GET
                     foreach ($matches as $key => $match) {
@@ -40,23 +39,25 @@ class Router {
                         }
                     }
 
-                    return $routeData['controller'];
+                    return [$routeData[0], isset($routeData[1]) ? $routeData[1] : 'index'];
                 }
             }
         }
 
-        // Check for static routes
-        if (array_key_exists($path, $this->routes)) {
-            if (empty($this->routes[$path]['roles']) || in_array($role, $this->routes[$path]['roles'])) {
-                // Parse the query string and store it in $_GET
-                parse_str($queryString, $_GET);
+        // Split the path into controller and method
+        $parts = explode('/', $path);
+        $controller = $parts[0];
+        $method = isset($parts[1]) ? $parts[1] : 'index';
 
-                return $this->routes[$path]['controller'];
+        // Check for static routes
+        if (array_key_exists($controller, $this->routes)) {
+            if (empty($this->routes[$controller]['roles']) || in_array($role, $this->routes[$controller]['roles'])) {
+                return [$this->routes[$controller]['controller'], $method];
             } else {
-                return 'errors';
+                return ['errors', 'index'];
             }
         }
 
-        return 'errors';
+        return ['errors', 'index'];
     }
 }
