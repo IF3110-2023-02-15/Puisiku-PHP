18675b07078df74e07bdf1f98c3bd70ef8852706
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-01 20:36:44 +0700

    feat: poems controller

---
+++ src/controllers/poems_controller.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once 'controller.php';
+require_once SERVICES_DIR . 'poems/index.php';
+require_once VIEWS_DIR . 'components/poems/index.php';
+require_once VIEWS_DIR . 'components/pagination.php';
+
+class Poems extends Controller {
+    public function index($method = null) {
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $this->loadView();
+                break;
+            default:
+                $this->methodNotAllowed();
+                break;
+        }
+    }
+
+    public function loadView() {
+        $current_page = 'Poems';
+        $playlists = [];
+        $role = $_SESSION['role'];
+        $display_search = true;
+        $profile_url = isset($_SESSION['profile_url']) ? $_SESSION['profile_url'] : '/img/queencard.jpeg';
+
+        $this->view('poems/index', ['current_page' => $current_page, 'playlists'=> $playlists, 'role' => $role, 'display_search' => $display_search, 'profile_url' => $profile_url]);
+    }
+
+    public function search() {
+        if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+            $this->methodNotAllowed();
+            return;
+        }
+
+        header('Content-Type: application/json');
+
+        $searchKey = isset($_GET['search_query']) ? $_GET['search_query'] : null;
+        $genre = isset($_GET['genre']) ? json_decode($_GET['genre']) : null;
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        $poemsService = new PoemsService();
+        $results = $poemsService->search($searchKey, $genre, $page);
+        $totalPages = $results['total_pages'];
+
+        $poems = poems($results['poems']);
+        $pagination = pagination($totalPages > 0 ? $page : 0 , $totalPages);
+
+        // Put the poems and pagination into an associative array
+        $response = [
+            'poems' => $poems,
+            'pagination' => $pagination
+        ];
+
+        // Convert the array to a JSON string and echo it
+        echo json_encode($response);
+    }
+
+    public function recommendation() {
+        if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+            $this->methodNotAllowed();
+            return;
+        }
+
+
+    }
+}
