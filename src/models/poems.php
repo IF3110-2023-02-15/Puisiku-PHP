d4768a74fadca501401c0357f6d311d2ec45418c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-04 19:27:52 +0700

    chore: add functionality to search with year query and sort

--- src/models/poems.php
+++ src/models/poems.php
@@ -5,15 +5,19 @@ require_once SRC_DIR . 'database/psql.php';
 
 class PoemsModel extends Models {
 
-    public function findPoems($searchKey = null, $genre = null, $page = 1, $poemsPerPage = 8, $sortBy = 'id DESC') {
-        // Start building the query
-        $query = "SELECT * FROM Poems";
+    public function findPoems($searchKey = null, $genre = null, $year_query = null, $sortBy = 'Poems.created_at DESC', $page = 1, $poemsPerPage = 6) {
+        // Split sortBy into column and direction
+        list($sortColumn, $sortDirection) = explode(' ', $sortBy);
 
-        // Add conditions for search key and genre if they're set
+        // Start building the query
+        $query = "SELECT * FROM Poems JOIN Users ON Poems.creator_id = Users.id ";
+
+        // Add conditions for search key, genre and year if they're set
         $conditions = [];
         $params = [];
         if ($searchKey !== null) {
-            $conditions[] = "LOWER(title) LIKE LOWER(?)";
+            $conditions[] = "(LOWER(title) LIKE LOWER(?) OR LOWER(Users.username) LIKE LOWER(?))";
+            $params[] = "%{$searchKey}%";
             $params[] = "%{$searchKey}%";
         }
         if ($genre !== null && count($genre) > 0) {
@@ -21,12 +25,22 @@ class PoemsModel extends Models {
             $conditions[] = "genre IN ($placeholders)";
             $params = array_merge($params, $genre);
         }
+        if ($year_query !== null) {
+            if (count($year_query) == 2) {
+                // If there are two elements in year_query, use them as the upper and lower bounds
+                $conditions[] = "year <= ? AND year >= ?";
+            } else if (count($year_query) == 1) {
+                // If there is one element in year_query, use it as the upper bound
+                $conditions[] = "year < ?";
+            }
+            array_push($params, ...$year_query);
+        }
         if (!empty($conditions)) {
             $query .= " WHERE " . implode(" AND ", $conditions);
         }
 
         // Add sorting
-        $query .= " ORDER BY " . $sortBy;
+        $query .= " ORDER BY " . $sortColumn . " " . $sortDirection;
 
         // Add pagination
         $offset = ($page - 1) * $poemsPerPage;
@@ -35,19 +49,27 @@ class PoemsModel extends Models {
         // Add pagination parameters
         array_push($params, $poemsPerPage, $offset);
 
-        // Execute the query and return the results
-        return $this->db->query($query, $params)->fetchAll(PDO::FETCH_ASSOC);
+        try {
+            $result = $this->db->query($query, $params);
+            return $result->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 
-    public function countPoems($searchKey = null, $genre = null) {
-        // Start building the query
-        $query = "SELECT COUNT(*) FROM Poems";
 
-        // Add conditions for search key and genre if they're set
+
+
+    public function countPoems($searchKey = null, $genre = null, $year_query = null) {
+        // Start building the query
+        $query = "SELECT COUNT(*) FROM Poems JOIN Users ON Poems.creator_id = Users.id";
+
+        // Add conditions for search key, genre and year if they're set
         $conditions = [];
         $params = [];
         if ($searchKey !== null) {
-            $conditions[] = "LOWER(title) LIKE LOWER(?)";
+            $conditions[] = "(LOWER(title) LIKE LOWER(?) OR LOWER(Users.username) LIKE LOWER(?))";
+            $params[] = "%{$searchKey}%";
             $params[] = "%{$searchKey}%";
         }
         if ($genre !== null && count($genre) > 0) {
@@ -55,13 +77,28 @@ class PoemsModel extends Models {
             $conditions[] = "genre IN ($placeholders)";
             $params = array_merge($params, $genre);
         }
+        if ($year_query !== null) {
+            if (count($year_query) == 2) {
+                // If there are two elements in year_query, use them as the upper and lower bounds
+                $conditions[] = "year <= ? AND year >= ?";
+            } else if (count($year_query) == 1) {
+                // If there is one element in year_query, use it as the upper bound
+                $conditions[] = "year < ?";
+            }
+            array_push($params, ...$year_query);
+        }
         if (!empty($conditions)) {
             $query .= " WHERE " . implode(" AND ", $conditions);
         }
 
         // Execute the query and return the result
-        return (int)$this->db->query($query, $params)->fetchColumn();
+        try {
+            return (int)$this->db->query($query, $params)->fetchColumn();
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
+
 
     public function findById($id) {
         $sql = 'SELECT Poems.*, 
