dd857c04d01c53139d0aa37a5f610ac28107ba03
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-05 16:07:12 +0700

    feat: Playlist Model

--- src/models/playlists.php
+++ src/models/playlists.php
@@ -1,1 +1,18 @@
 <?php
+
+require_once 'models.php';
+
+class PlaylistsModel extends Models {
+
+    public function getUserPlaylists($userId) {
+        $sql = 'SELECT id, title, image_path FROM Playlists WHERE owner_id = ?';
+
+        return $this->db->query($sql, [$userId])->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getPlaylistData($playlistId) {
+        $sql = 'SELECT id, title, image_path, DATE(updated_at) FROM Playlists WHERE id = ?';
+
+        return $this->db->query($sql, [$playlistId])->fetch(PDO::FETCH_ASSOC);
+    }
+}
