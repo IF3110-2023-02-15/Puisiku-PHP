5b51411478d04394bd540374bb12cdddc741c8ea
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-05 16:05:47 +0700

    refactor: use the getSidebarNavbarInfo

--- src/controllers/home_controller.php
+++ src/controllers/home_controller.php
@@ -1,20 +1,25 @@
 <?php
 
 require_once 'controller.php';
+require_once SERVICES_DIR . 'playlists/index.php';
 
 class Home extends Controller {
     public function index() {
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            $this->loadView();
+        switch($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $this->loadView();
+                break;
+            default:
+                $this->methodNotAllowed();
+                break;
         }
     }
 
     private function loadView() {
         $current_page = 'Home';
-        $playlists = [];
-        $role = $_SESSION['role'];
         $display_search = false;
-        $profile_url = isset($_SESSION['profile_url']) ? $_SESSION['profile_url'] : '/img/queencard.jpeg';
+
+        list($role, $profile_url, $playlists) = $this->getSidebarNavbarInfo();
 
         $this->view('home/index', ['current_page' => $current_page, 'playlists'=> $playlists, 'role' => $role, 'display_search' => $display_search, 'profile_url' => $profile_url]);
     }
