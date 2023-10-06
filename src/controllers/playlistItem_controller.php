69dc52ed3c84739257171ac5ccbb826ecc2aaf03
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-06 23:30:22 +0700

    chore: delete playlist item controller

--- src/controllers/playlistItem_controller.php
+++ src/controllers/playlistItem_controller.php
@@ -2,6 +2,7 @@
 
 require_once 'controller.php';
 require_once SERVICES_DIR . 'playlists/index.php';
+require_once VIEWS_DIR . 'components/table.php';
 
 class PlaylistItem extends Controller {
 
@@ -10,13 +11,16 @@ class PlaylistItem extends Controller {
             case 'POST':
                 $this->add();
                 break;
+            case 'DELETE':
+                $this->delete();
+                break;
             default:
                 $this->methodNotAllowed();
                 break;
         }
     }
 
-    public function add() {
+    private function add() {
         $playlistId = isset($_POST['playlistId']) ? $_POST['playlistId'] : null;
         $poemId = isset($_POST['poemId']) ? $_POST['poemId'] : null;
 
@@ -26,6 +30,26 @@ class PlaylistItem extends Controller {
             $result = $playlistsService->addPlaylistItem($playlistId, $poemId);
 
             echo json_encode(['success' => 'Successfully added poem to playlist!', 'result' => $result]);
+        } catch (Exception $e) {
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+    }
+
+    private function delete() {
+        $data = $this->getData();
+
+        $playlistId = $data['playlistId'];
+        $poemId = $data['poemId'];
+
+        $playlistsService = new PlaylistsService();
+
+        try {
+            $result = $playlistsService->deletePlaylistItem($playlistId, $poemId);
+            $playlists = $playlistsService->getPlaylistItems($playlistId);
+
+            $table = createTable(['#', 'Title', 'Creator', 'Genre', 'Year', 'Delete'], $playlists);
+
+            echo json_encode(['success' => $result, 'table' => $table]);
         } catch (Exception $e) {
             echo json_encode(['error' => $e->getMessage()]);
         }
