b579dbc2a862ef2a995825114ab7f8fcc6befddc
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-07 01:00:12 +0700

    chore: use landing for css

--- src/views/pages/landing/index.php
+++ src/views/pages/landing/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="/css/landingpage.css">
+    <link rel="stylesheet" href="/css/landing.css">
 </head>
 <body id="grad">
     <div class="center">
