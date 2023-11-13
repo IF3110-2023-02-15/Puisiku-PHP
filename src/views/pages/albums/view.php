1cda928997b162cd4d26252fdc4732f34db69de8
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-14 03:22:00 +0700

    feat: premium creator

---
+++ src/views/pages/albums/view.php
@@ -0,0 +1,42 @@
+<?php
+    $creator = $creatorData->creator;
+    $creatorImagePath = getenv('REST_PUBLIC_BASE_URL') . $creator->imagePath;
+    $creatorName = $creator->name;
+    $creatorDescription = $creator->description;
+
+    $albums = $creatorData->albums;
+?>
+
+<div class="premium-creator">
+    <div class="creator-profile">
+        <div class="image-wrapper">
+            <img alt="creator image" src="<?php echo $creatorImagePath; ?>" />
+        </div>
+        <div class="profile-wrapper">
+            <h1>
+                <?php echo $creatorName; ?>
+            </h1>
+            <p>
+                <?php echo $creatorDescription; ?>
+            </p>
+        </div>
+    </div>
+
+    <hr />
+
+    <div class="creator-albums">
+        <?php
+        require_once VIEWS_DIR . '/components/premium/album_box.php';
+
+        foreach($albums as $album) {
+            $albumId = $album->id;
+            $albumName = $album->name;
+            $albumImagePath = getenv('REST_PUBLIC_BASE_URL') . $album->imagePath;
+
+            echo "<a href='/premium/album/" . $albumId . "'>";
+            echo albumBox($albumName, $albumImagePath);
+            echo "</a>";
+        }
+        ?>
+    </div>
+</div>
