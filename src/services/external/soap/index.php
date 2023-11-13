67ffd06a77370d2b08631d8d9d9cfa52b21a2b11
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-14 03:20:22 +0700

    feat: soap service

---
+++ src/services/external/soap/index.php
@@ -0,0 +1,27 @@
+<?php
+
+class SOAPService {
+    private $client;
+
+
+    public function __construct($endpoint)
+    {
+        $baseUrl = getenv('SOAP_BASE_URL');
+        $opts = array(
+            'http' => array(
+                'header' => 'x-api-key: ' . getenv('SOAP_API_KEY'),
+            ),
+        );
+        $context = stream_context_create($opts);
+        $this->client = new SoapClient($baseUrl . $endpoint . "?wsdl", ['stream_context' => $context]);
+    }
+
+    public function call($method, $params) {
+        try {
+            $result = $this->client->__soapCall($method, [$params]);
+            return $result;
+        } catch (SoapFault $fault) {
+            echo "SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})";
+        }
+    }
+}
