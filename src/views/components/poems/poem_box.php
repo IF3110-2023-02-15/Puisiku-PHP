80fac9433cf4f799efa67868885e8867e0763e8d
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-14 20:33:27 +0700

    refactor: decompose poem_box into function

--- src/views/components/poems/poem_box.php
+++ src/views/components/poems/poem_box.php
@@ -1,5 +1,9 @@
-<div class="poem" onclick="window.location='/poem/<?php echo $id; ?>'">
-    <img src="<?php echo $img; ?>" alt="" class="poem-img">
-    <h1><?php echo $title; ?></h1>
-    <h2><?php echo $creator; ?></h2>
-</div>
+<?php
+function poemBox($onClick, $img, $title, $creator) {
+    echo '<div class="poem" onclick="window.location=\'' . $onClick . '\'">';
+    echo '<img src="' . $img . '" alt="" class="poem-img">';
+    echo '<h1>' . $title . '</h1>';
+    echo '<h2>' . $creator . '</h2>';
+    echo '</div>';
+}
+
